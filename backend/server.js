c9a909f792e71d79a150c66586f29248074ab875
rodrigorunner <rodrigoregisrun@gmail.com>
2022-09-20 01:18:15 -0300

    removendo arquivos estaticos

--- backend/server.js
+++ backend/server.js
@@ -18,14 +18,6 @@ app.use(express.urlencoded({extended: false}))
 // Configurar rota default da aplicação.
 app.use('/api/v1/', require('./routes/runRoutes'))
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => res.send('Please set to production'))
-}
-
 app.use(handleError)
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
