5275fa6c27c0deca92f0ce1cf91ac7e533174936
rodrigorunner <rodrigoregisrun@gmail.com>
2022-11-29 16:00:45 -0300

    refatoração

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,55 +1,45 @@
-const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const ExpressError = require('../utils/ExpressError')
+const catchAsync = require('../utils/catchAsync')
+const Joi = require('joi')
 
-const getUser = asyncHandler(async (req, res) => {
+const getUser = async (req, res) => {
     const user = await User.find({})
 
     res.status(200).json(user)
-})
+}
 
-const getUserById = asyncHandler(async (req, res) => {
+const getUserById = catchAsync(async (req, res) => {
     const user = await User.findById(req.params.id)
-
-    if(!user) {
-        res.status(400)
-        throw new Error('User does not exists.')
-    }
-
     res.status(200).json(user)
 })
 
-const createUser = asyncHandler(async (req, res) => {
-    if(!req.body.name || !req.body.username || !req.body.email || !req.body.website || !req.body.zipcode || !req.body.city) {
-        res.status(400)
-        throw new Error('Please, add a user.')
+const createUser = catchAsync(async (req, res) => {
+    const racesSchema = Joi.object({
+        name: Joi.string().required(),
+        username: Joi.string().required(),
+        email: Joi.string().required(),
+        city: Joi.string().required(),
+        zipcode: Joi.string().required(),
+        website: Joi.string().required(),
+    }).required()
+
+    const { error } = await racesSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message)
+        throw new ExpressError(msg, 404)
     }
-
-    const createdUser = await User.create({
-        name: req.body.name,
-        username: req.body.username,
-        email: req.body.email,
-        city: req.body.city,
-        zipcode: req.body.zipcode,
-        website: req.body.website,
-    })
-
-    res.status(200).json(createdUser)
+    const user = new User(req.body)
+    await user.save()
 })
 
-const updateUser = asyncHandler(async (req, res) => {
+const updateUser = async (req, res) => {
     const user = await User.findById(req.params.id)
-
-    if(!user) {
-        res.status(400)
-        throw new Error('User does not exists.')
-    }
-
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-
     res.status(200).json(updatedUser)
-})
+}
 
-const deleteUser = asyncHandler(async (req, res) => {
+const deleteUser = async (req, res) => {
     const user = await User.findById(req.params.id)
 
     if(!user) {
@@ -60,7 +50,7 @@ const deleteUser = asyncHandler(async (req, res) => {
     await user.remove()
 
     res.status(200).json({ id: req.params.id })
-}) 
+}
 
 module.exports = {
     getUser,
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const { handleError } = require('./middleware/userMiddleware')
+const ExpressError = require('./utils/ExpressError')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
@@ -6,7 +6,11 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const cors = require('cors')
+const ejs = require('ejs')
+const engine = require('ejs-mate')
 const app = express()
+const session = require('express-session')
+const { config } = require('dotenv')
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -16,10 +20,34 @@ connectDB()
 // Configurar middleware para tratar requisição POST
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', engine)
+
+const configCookie = {
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+}
+app.use(session(configCookie))
 
 // Configurar rota default da aplicação.
 app.use('/api/v1/', require('./routes/userRoutes'))
 
-app.use(handleError)
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err
+    console.log(err)
+    if(!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).render('error', { err })
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
